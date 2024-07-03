97f2276f2dcaca6cc6ccb8dd583741b136268a9b
ahmed <ahmedehab.aa47@gmail.com>
2024-07-03 20:51:42 +0300

    update

--- react_project/src/components/Footer/Footer.jsx
+++ react_project/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
-import { Stack, Box, Typography } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import Box1 from "./Box1.jsx";
 import Box2 from "./Box2.jsx";
 import Box3 from "./Box3.jsx";
 function Footer() {
     return (
 
-<>
-            <Box sx={{ mt: "81px", py: 3, px: 1, display: "flex", alignItems: "center", justifyContent: "space-between", borderTop: "2px solid #000", background: "#e72323", position:"relative" }}>
+        <>
+            <Box sx={{ mt: "81px", py: 3, px: 1, display: "flex", alignItems: "center", justifyContent: "space-between", borderTop: "2px solid #000", background: "#e72323", position: "relative" }}>
                 <Stack>
                     <Box1 />
                 </Stack>
@@ -20,7 +20,7 @@ function Footer() {
                 {/* <Typography sx={{ mt: 4, color: "red", textAlign: "center" }}>all right to @anadl </Typography> */}
 
             </Box>
-</>
+        </>
     )
 }
 
