ec91907b488d11f08b03288599b71feefde3a676
ahmed <ahmedehab.aa47@gmail.com>
2024-07-03 16:41:47 +0300

    mm

---
+++ react_project/src/App.jsx
@@ -0,0 +1,17 @@
+// import React from "react";
+import Header from "./components/header/Header";
+// import Hero from "./components/Hero/Hero"
+import Main from "./components/Main/Main";
+import Menu from "./components/Menu/Menu"
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Hero/> */}
+      {/* <Main /> */}
+<Menu/>
+    </>
+  );
+}
+
+export default App;
---
+++ react_project/src/components/Menu/Menu.jsx
@@ -0,0 +1,18 @@
+import { Stack, Container, Typography } from "@mui/material";
+import Box from "./box"
+function Menu() {
+  return (
+    <Container>
+      <Stack>
+        <Typography
+          sx={{ textAlign: "center", fontSize: "33px", fontWeight: "bold" }}
+        >
+          MENU
+        </Typography>
+        <Stack> <Box/> </Stack>
+      </Stack>
+    </Container>
+  );
+}
+
+export default Menu;
